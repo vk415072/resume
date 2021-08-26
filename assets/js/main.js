28e864908f06f4f55c9aa6291f1bd389c9147ac3
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-08-26 16:18:56 +0530

    hide menu after item toggle for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// SHOW MENU
+// SHOW & HIDE MENU
 const showMenu = (toggledId, navId) => {
    const toggle = document.getElementById(toggledId),
       nav = document.getElementById(navId);
@@ -11,5 +11,13 @@ const showMenu = (toggledId, navId) => {
       });
    }
 };
-
 showMenu("nav-toggle", "nav-menu");
+
+// HIDE MENU MOBILE (after item click)
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+   const navMenu = document.getElementById("nav-menu");
+   // when we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
